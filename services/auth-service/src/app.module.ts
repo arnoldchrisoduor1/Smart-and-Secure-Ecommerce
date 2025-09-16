9534f651965be0d77ab0a6ce8f54d2565ef3d33e
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-09-16 23:24:46 +0300

    just created the user entity

--- services/auth-service/src/app.module.ts
+++ services/auth-service/src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import Module from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ThrottleModule } from '@nestjs/cache-manager';
+import { RedisOptions } from 'ioredis';
+import * as redisStore from 'cache-manager-redis-store';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from '/users/users.module';
+import { SecurityModule } from './security/security.module';
+
+import { User } from './users/entities/user.entity';
+import { SecurityEvent } from './auth/entities/refresh-token.entity';
+import { RefreshToken } from './auth/entities/refresh-token.entity';
