864aa3382d82b7db21c57b7e5c9c623c31fe170b
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-09-17 18:58:29 +0300

    created the DTOs

--- services/auth-service/src/auth/strategies/local.strategy.ts
+++ services/auth-service/src/auth/strategies/local.strategy.ts
@@ -12,8 +12,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     }
     
     async validate(email: string, password: string): Promise<any> {
+        const user = await this.authService.validateUser(email, password);
         if(!user) {
-            throw new UnauthorizedException('invalid credentials');
+            throw new UnauthorizedException('Invalid credentials');
         }
         return user;
     }
